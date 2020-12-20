d09e788cc31acc31d42d1ed1472fb0191324f47b
Athavan Karunakaran <athavan.karunakaran@gmail.com>
2020-12-20 13:52:48 -0500

    Fixed lint errors from eslintrc file

--- minymal-chess/.eslintrc.js
+++ minymal-chess/.eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-    "extends": [
-        "airbnb-base",
-        "plugin:react/recommended",
-        "plugin:cypress/recommended"
+    extends: [
+        'airbnb-base',
+        'plugin:react/recommended',
+        'plugin:cypress/recommended'
     ],
-    "parser": "babel-eslint",
+    parser: 'babel-eslint',
     ignorePatterns: ['src/serviceWorker.js', 'build/**/*.js', 'cypress/integration/examples/*.js'],
-    "rules": {
-        "indent": ["error", 4],
-        "comma-dangle": ["error", "never"],
-        "quotes": ["error", "single", { "avoidEscape": true }],
-        "jsx-quotes": ["error", "prefer-single"],
-        "react/jsx-no-literals": ["error"],
-        "react/jsx-indent" : ["error", 4],
-        "react/jsx-indent-props": ["error", 4],
-        "max-lines": ["warn", 80]
+    rules: {
+        indent: ['error', 4],
+        'comma-dangle': ['error', 'never'],
+        quotes: ['error', 'single', { avoidEscape: true }],
+        'jsx-quotes': ['error', 'prefer-single'],
+        'react/jsx-no-literals': ['error'],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'max-lines': ['warn', 80]
     },
-    "env": {
-        "browser": true,
-        "jest": true
+    env: {
+        browser: true,
+        jest: true
     }
 };
